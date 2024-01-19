88f4613b5c5d5aadc36c739034fc6cc6cbdb4e41
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-01-20 01:31:30 +0600

    featured jobs display all

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Job from "../Job/Job";
+import "./FeaturedJobs.css";
 
 const FeaturedJobs = () => {
   const [Jobs, setJobs] = useState([]);
@@ -10,17 +11,20 @@ const FeaturedJobs = () => {
   }, []);
   return (
     <div>
-      <div>
+      <div className="categories-title">
         <h1>Featured Jobs</h1>
         <p>
           Explore thousands of job opportunities with all the information you
           need. Its your future
         </p>
       </div>
-      <div>
+      <div className="job-post-container">
         {Jobs.map((job) => (
           <Job job={job} key={job.id}></Job>
         ))}
+      </div>
+      <div className="see-all">
+        <button className="btn-see-all">See All Jobs</button>
       </div>
     </div>
   );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "./Job.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDollarSign, faLocationPin } from "@fortawesome/free-solid-svg-icons";
 
 const Job = ({ job }) => {
   const {
     logo,
-    comopany_name,
+    company_name,
     location,
     job_type,
     salary,
@@ -11,14 +14,29 @@ const Job = ({ job }) => {
     job_title,
   } = job;
   return (
-    <div>
+    <div className="job-post-item">
       <img src={logo} alt="" />
       <h2>{job_title}</h2>
-      <p>{comopany_name}</p>
-      <p>{remote_or_onsite}</p>
-      <p>{job_type}</p>
-      <p>{location}</p>
-      <p>{salary}</p>
+      <p className="company-name">{company_name}</p>
+      <div className="job-type">
+        <p>{remote_or_onsite}</p>
+        <p>{job_type}</p>
+      </div>
+      <div className="job-location-salary">
+        <p>
+          <span className="icon">
+            <FontAwesomeIcon icon={faLocationPin}></FontAwesomeIcon>
+          </span>{" "}
+          {location}
+        </p>
+        <p>
+          <span className="icon">
+            <FontAwesomeIcon icon={faDollarSign}></FontAwesomeIcon>{" "}
+          </span>{" "}
+          {salary}
+        </p>
+      </div>
+      <button className="btn-view-details">View Details</button>
     </div>
   );
 };
