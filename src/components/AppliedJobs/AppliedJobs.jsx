1239935f222ff411cd314974747b471e26364a3b
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-01-21 19:28:25 +0600

    get data from local storage

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -3,30 +3,33 @@ import { getShoppingCart } from "../../utilities/fakeDb";
 import { useLoaderData } from "react-router-dom";
 
 const AppliedJobs = () => {
-  const [jobs, setJobs] = useState([]);
-  // const jobs = useLoaderData();
+  // const [jobs, setJobs] = useState([]);
+  const jobs = useLoaderData();
   const [applied, SetApplied] = useState([]);
+  // console.log(applied);
 
-  useEffect(() => {
-    fetch("jobs.json")
-      .then((res) => res.json())
-      .then((data) => setJobs(data));
-  }, []);
-
+  // useEffect(() => {
+  //   fetch("jobs.json")
+  //     .then((res) => res.json())
+  //     .then((data) => setJobs(data));
+  // }, []);
   useEffect(() => {
     const storedJobs = getShoppingCart();
+    let savedApplied = [];
+
     for (const id in storedJobs) {
+      const idNumber = parseInt(id);
       // console.log(typeof id);
-      const appliedJobs = jobs.find((job) => job.id == id);
-      const newAppliedJob = [...applied, appliedJobs];
-      SetApplied(newAppliedJob);
+      const appliedJobs = jobs.find((job) => job.id === idNumber);
+      if (appliedJobs) {
+        const apply = storedJobs[id];
+        appliedJobs.apply = apply;
+        savedApplied.push(appliedJobs);
+      }
     }
+    SetApplied(savedApplied);
   }, [jobs]);
-  return (
-    <div>
-      <h1>Applied Jobs will be here!! {applied.length}</h1>
-    </div>
-  );
+  return <div>{<h1>Applied Jobs will be here!! </h1>}</div>;
 };
 
 export default AppliedJobs;
